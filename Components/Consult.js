002cf1b811d373ee8842313691cb3165a13440fb
Shruti Jha <122458889+Shruti-jha6@users.noreply.github.com>
2024-03-31 21:10:04 +0530

    Add files via upload

---
+++ Components/Consult.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Consult() {
+  return (
+    <div>
+        Find NGO !
+      
+    </div>
+  )
+}
+
+export default Consult
---
+++ Components/Profile.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Profile() {
+  return (
+    <div>
+      YOur Profile is here
+    </div>
+  )
+}
+
+export default Profile
